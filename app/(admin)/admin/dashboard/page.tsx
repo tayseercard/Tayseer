922f969e60ac06f04fca0e56b6cb0ef0040c33ae
tayseer <tayseercard@gmail.com>
2025-11-05 10:57:22 +0100

    UI31

--- app/(admin)/admin/dashboard/page.tsx
+++ app/(admin)/admin/dashboard/page.tsx
@@ -92,7 +92,7 @@ export default function AdminDashboardPage() {
 
   /* ---------- UI ---------- */
   return (
-<div className="h-screen w-screen overflow-hidden bg-gradient-to-br from-white via-gray-50 to-emerald-50 text-gray-900 px-4 py-8 sm:px-6 lg:px-10 flex flex-col justify-between">
+<div className="h-screen w-screen overflow-hidden bg-gradient-to-br from-white via-gray-50 to-emerald-50 text-gray-900 px-4 py-8 sm:px-6 lg:px-10 flex flex-col justify-center items-center">
       {/* HEADER */}
       <motion.header
         initial={{ opacity: 0, y: -10 }}
